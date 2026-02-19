89516b9efe72fc2df6a409b5ffb0f684bd1aa07d
Avost-off <noasueur879@gmail.com>
2026-02-19 21:47:48 +0100

    Delete panel/dashboard/members/prison.py

--- panel/dashboard/members/prison.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-prison_bp = Blueprint("prison", __name__, url_prefix="/moderation/prison")
-
-@prison_bp.route("/")
-def prison():
-    return render_template("moderation/prison.html")
